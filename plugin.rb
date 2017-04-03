2ddc92eeac44210629df88d0c331433fbb1582a7
Janno Liivak <janno@edev.ee>
2017-04-03 12:49:20 +0300

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: post-information
+# about: Plugin that let's moderators and admins to look at post's user IP info
+# version: 0.0.1
+# authors: Janno Liivak
+
+enabled_site_setting :post_information_enabled
